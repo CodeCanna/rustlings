ef4c98022d95ff13509b65815eb9796fa29cf6b1
CodeCanna <codecannamw@gmail.com>
2024-09-21 08:48:02 -0600

    Got through move_semantics

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -19,8 +19,8 @@ mod tests {
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
-
-        let vec1 = fill_vec(vec0);
+        let vec1: Vec<i32> = fill_vec(vec0.clone()); // .to_owned() was the answer but I don't know why...
+        
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
