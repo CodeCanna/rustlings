6c33bbd7d1a98bb2126cde2730f938273843376a
CodeCanna <codecannamw@gmail.com>
2024-09-20 21:16:45 -0600

    working on vec2

--- exercises/04_primitive_types/primitive_types5.rs
+++ exercises/04_primitive_types/primitive_types5.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let cat = ("Furry McFurson", 3.5);
+    let _cat = ("Furry McFurson", 3.5);
 
     // TODO: Destructure the `cat` tuple in one statement so that the println works.
-    // let /* your pattern here */ = cat;
+    let (name, age) = ("Furry McFurson", 3.5);
 
     println!("{name} is {age} years old");
 }
